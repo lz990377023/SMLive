bb6bdf445a442939936aadc3ce1ea423406ede0e
lz990377023 <990377023@qq.com>
2018-03-09 11:32:01 +0800

    Initial Commit

---
+++ SMLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMLive
+//
+//  Created by lizhan on 2018/3/9.
+//  Copyright © 2018年 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SMLive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SMLive
+//
+//  Created by lizhan on 2018/3/9.
+//  Copyright © 2018年 sam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
